d8ce04784368d1df9b665aa82b1c0d5322207b74
sumity29 <enc.sumitpal29@gmail.com>
2026-03-31 18:47:57 +0530

    creating get messages in controller with pagination

--- src/main/java/com/sumit/chat_app_backend/controllers/RoomController.java
+++ src/main/java/com/sumit/chat_app_backend/controllers/RoomController.java
@@ -1,10 +1,13 @@
 package com.sumit.chat_app_backend.controllers;
 
+import com.sumit.chat_app_backend.entities.Message;
 import com.sumit.chat_app_backend.entities.Room;
 import com.sumit.chat_app_backend.repositories.RoomRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/rooms")
@@ -57,5 +60,27 @@ public class RoomController {
        }
 
        return ResponseEntity.ok(room);
+    }
+
+    @GetMapping("/{roomId}/messages")
+    public ResponseEntity<List<Message>> getMessages(
+        @PathVariable String roomId,
+        @RequestParam(value = "page", defaultValue = "0", required = false) int page,
+        @RequestParam(value = "size", defaultValue = "20", required = false) int size
+    ){
+
+        Room room = roomRepository.findByRoomId(roomId);
+
+        if(room == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        // get message
+        List<Message> messages = room.getMessages();
+        int start = Math.max(0, messages.size() - (page + 1) * size);
+        int end = Math.min(messages.size(), start + size);
+
+        List<Message> paginatedMessages =  messages.subList(start, end);
+        return ResponseEntity.ok(paginatedMessages);
     }
 }
