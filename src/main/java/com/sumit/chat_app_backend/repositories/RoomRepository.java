d5e4b8701817ed5791b62956a186c161ac8620af
sumity29 <enc.sumitpal29@gmail.com>
2026-03-31 12:22:56 +0530

    creating room repository

---
+++ src/main/java/com/sumit/chat_app_backend/repositories/RoomRepository.java
@@ -0,0 +1,11 @@
+package com.sumit.chat_app_backend.repositories;
+
+import com.sumit.chat_app_backend.entities.Room;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends MongoRepository<Room, String> {
+    // get room  by roomId;
+    Room findByRoomId(String roomId);
+}
