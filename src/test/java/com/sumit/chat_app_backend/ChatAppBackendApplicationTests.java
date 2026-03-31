ec18b55d869f39d2691bc0641fa809cbe06a169f
sumity29 <enc.sumitpal29@gmail.com>
2026-03-31 11:33:05 +0530

    initiall code

---
+++ src/test/java/com/sumit/chat_app_backend/ChatAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumit.chat_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
